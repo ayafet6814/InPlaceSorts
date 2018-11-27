bb56a500e6811fc0c4be09de20527b9c3c6edc0f
ayafet6814 <43213084+ayafet6814@users.noreply.github.com>
2018-11-27 11:46:02 -0500

    Update InPlaceSorts.java

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -59,4 +59,31 @@ public class InPlaceSorts
             }
         }
     }
+    
+    public static void quickSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int pivot = partition(arr, left, right);
+
+            quickSort(arr, left, pivot-1);
+            quickSort(arr, pivot+1, right);
+        }
+    }
+
+    public static int partition(int[] arr, int left, int right)
+    {
+        int pivot = arr[right]; int i = left-1;
+        for (int j = left; j <= right; j++)
+        {
+            if (arr[j] <= pivot)
+            {
+                i++;
+                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+            }
+        }
+        int eTemp = arr[i+1]; arr[i+1] = arr[right]; arr[right] = eTemp;
+        return i+1;
+    }
+    
 }
